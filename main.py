9bd79de71a32dd82097b818b228a4326dbb2396c
Rogério Soares <rogerio_si@yahoo.com.br>
2021-06-14 19:03:42 +0000

    teste3

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template
+import time
 
 # Create a flask app
-app = Flask(
-  __name__,
-  template_folder='templates',
-  static_folder='static'
-)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
 
 # Index page (now using the index.html file)
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
 
 if __name__ == '__main__':
-  # Run the Flask app
-  app.run(
-	host='0.0.0.0',
-	debug=True,
-	port=8080
-  )
+    # Run the Flask app
+    app.run(host='0.0.0.0', debug=True, port=8080)
